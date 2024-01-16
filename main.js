0d37b4a687e86a0628a2390a043fec9e2346e384
Mattias Schertell <schertell@gmail.com>
2024-01-16 11:31:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,18 @@
+/*
+Det är ok att använda den här filen i din lösning.
+
+scriptet ska "köra" din lösningen inklusive testutskrifter av objekten.
+*/
+
 const User = require("./User");
 
 
-// Exempel
+// Exempeldata
 const user1 = new User("Alice", "alice@email.com", "password123");
 const user2 = new User("Bob", "bob@email.com", "pass456");
 
 const post1 = user1.createBlogPost("JavaScript Basics", "An introduction to JavaScript programming.");
+//användare 2 lägger till en kommentar till användare 1 post
 user2.addComment(post1, "Great post! I learned a lot.");
 
 const post2 = user2.createBlogPost("Node.js: Getting Started", "Setting up a Node.js environment.");
