6678f9f5bc5f2e6ff35fdbaf105b6ee0ce19a786
Mattias Schertell <schertell@gmail.com>
2024-01-16 11:40:04 +0100

    Update main.js

--- main.js
+++ main.js
@@ -25,3 +25,29 @@ post1.displayPost();
 user2.displayInfo();
 console.log("\nUser 2's Blog Posts:");
 post2.displayPost();
+
+/*
+Exempel på körning av det här scriptet (node main.js) så skulle följande skrivas ut i konsollen/terminalen:
+
+Username: Alice, Email: alice@email.com
+
+User 1's Blog Posts:
+Title: JavaScript Basics
+Author: Alice
+Content: An introduction to JavaScript programming.
+
+Comments:
+Author: Bob
+Comment: Great post! I learned a lot.
+
+Username: Bob, Email: bob@email.com
+
+User 2's Blog Posts:
+Title: Node.js: Getting Started
+Author: Bob
+Content: Setting up a Node.js environment.
+
+Comments:
+Author: Alice
+Comment: Nice tutorial. Looking forward to more!
+*/
